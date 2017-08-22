8e4030a453dd777c22f4b20ce6a70cafd56b9088
Witty Chang <wcc1213@gmail.com>
2017-08-22 10:00:08 -0700

    Create About route.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders }  from '@angular/core'; // Provides this router to the rest of the app
 import { Routes, RouterModule } from '@angular/router'; // Render specific components when specific urls are provided to the router
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 // Master list of all available routes, contains values that other code in the app cannot change
 const appRoutes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
